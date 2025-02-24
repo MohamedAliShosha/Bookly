7d10c593d741d859a2f01b4aa030497c053dc8b2
Mohamed Ali <a74498722@gmail.com>
2025-02-24 14:00:02 -0800

    feat: add splash view route to app router

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,15 +1,26 @@
 import 'package:book_store_app/features/home/home_view.dart';
+import 'package:book_store_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
 
   static final routes = [
-    GoRouter(routes: [
-      GoRoute(
-        path: kHomeView,
-        builder: (context, state) => const HomeView(),
-      ),
-    ]),
+    GoRouter(
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) => const SplashView(),
+        ),
+      ],
+    ),
+    GoRouter(
+      routes: [
+        GoRoute(
+          path: kHomeView,
+          builder: (context, state) => const HomeView(),
+        ),
+      ],
+    ),
   ];
 }
