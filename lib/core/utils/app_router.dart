805f67ec3f313afc935cfc2201803f556587ff99
Mohamed Ali <a74498722@gmail.com>
2025-02-25 22:28:37 -0800

    feat: add DetailsView and update app router for navigation

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,10 +1,11 @@
+import 'package:book_store_app/features/details/presentation/views/details_view.dart';
 import 'package:book_store_app/features/home/presentation/views/home_view.dart';
 import 'package:book_store_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
-
+  static const kDetailsView = '/detailsView';
   // static used to make the method accessible without creating an instance of the class
   static final router = GoRouter(
     routes: [
@@ -16,6 +17,11 @@ abstract class AppRouter {
         path:
             kHomeView, // The path that will be used to navigate to the home screen
         builder: (context, state) => const HomeView(),
+      ),
+      GoRoute(
+        path:
+            kHomeView, // The path that will be used to navigate to the home screen
+        builder: (context, state) => const DetailsView(),
       ),
     ],
   );
