82eacf5e5a356fd187edee4fd6f4a760734e6096
Mohamed Ali <a74498722@gmail.com>
2025-02-25 23:24:30 -0800

    feat: enhance DetailsView layout with improved styling and additional book information

--- lib/features/details/presentation/views/details_view.dart
+++ lib/features/details/presentation/views/details_view.dart
@@ -1,7 +1,9 @@
 import 'package:book_store_app/constants.dart';
+import 'package:book_store_app/core/utils/styles.dart';
 import 'package:book_store_app/features/home/widgets/custom_app_bar.dart';
 import 'package:book_store_app/features/home/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DetailsView extends StatelessWidget {
   const DetailsView({super.key});
@@ -19,17 +21,64 @@ class DetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Padding(
+          const Padding(
             padding: EdgeInsets.symmetric(horizontal: 30),
             child: CustomAppBar(),
           ),
-          CustomBookImage(
-            imageUrl: kBookImageUrl,
-          )
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 50),
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * .4,
+              child: const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 8.0),
+                child: CustomBookImage(
+                  imageUrl: kBookImageUrl,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Jungle Book',
+                style: Styles.txtStyle20.copyWith(
+                  fontFamily: GoogleFonts.ruslanDisplay().fontFamily,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.txtStyle14.copyWith(
+                  fontFamily: GoogleFonts.ruslanDisplay().fontFamily,
+                ),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                maxLines: 6,
+                overflow: TextOverflow.ellipsis,
+                'Description: The Jungle Book is a collection of stories by the English author Rudyard Kipling. Most of the characters are animals such as Shere Khan the tiger and Baloo the bear, though a principal character',
+                style: Styles.txtStyle14.copyWith(
+                  fontFamily: GoogleFonts.ruslanDisplay().fontFamily,
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
