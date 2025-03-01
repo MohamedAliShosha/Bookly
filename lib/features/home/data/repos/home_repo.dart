d302b4d5f93393159f160a55a9146ceb860e41de
Mohamed Ali <a74498722@gmail.com>
2025-02-28 17:58:36 -0800

    feat: add dartz dependency and create empty failures and home_repo files

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,8 @@
+import 'package:book_store_app/core/errors/failures.dart';
+import 'package:book_store_app/features/home/data/models/book_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failures, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
+}
