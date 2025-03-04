ad203aa11388c4309a5577a6efb69327edf2424f
Mohamed Ali <a74498722@gmail.com>
2025-03-04 14:47:49 -0800

    implement FeaturedBooksCubit and state management for featured books

---
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_state.dart
@@ -0,0 +1,27 @@
+part of 'featured_books_cubit.dart';
+
+sealed class FeaturedBooksState extends Equatable {
+  const FeaturedBooksState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class FeaturedBooksInitial extends FeaturedBooksState {}
+
+final class FeaturedBooksLoading extends FeaturedBooksState {}
+
+final class FeaturedBooksSuccess extends FeaturedBooksState {
+  final List<BookModel> books;
+
+  const FeaturedBooksSuccess(this.books);
+
+  @override
+  List<Object> get props => [books];
+}
+
+final class FeaturedBooksFailure extends FeaturedBooksState {
+  final String message;
+
+  const FeaturedBooksFailure(this.message);
+}
