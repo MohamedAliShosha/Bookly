7ac7456a7ab6defa8448bd8c6e13aff5ef25299a
Mohamed Ali <a74498722@gmail.com>
2025-03-08 16:40:49 -0800

    implement service locator for dependency management and add get_it package

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,23 @@
+import 'package:book_store_app/core/utils/api_service.dart';
+import 'package:book_store_app/features/home/data/repos/home_repo_implement.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+void setUpServiceLocator() {
+  // Register the ApiService as a singleton
+  getIt.registerSingleton<ApiService>(
+    ApiService(
+      Dio(),
+    ),
+  );
+
+  // Register your services here with identifying the service name inside <>
+  getIt.registerSingleton<HomeRepoImplement>(
+    HomeRepoImplement(
+      getIt.get<
+          ApiService>(), // Locating the ApiService instance inside the HomeRepoImplement
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:book_store_app/constants.dart';
+import 'package:book_store_app/core/utils/api_service.dart';
 import 'package:book_store_app/core/utils/app_router.dart';
+import 'package:book_store_app/features/home/data/repos/home_repo_implement.dart';
+import 'package:book_store_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:book_store_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class BookStore extends StatelessWidget {
@@ -8,15 +14,38 @@ class BookStore extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      theme: ThemeData(
-        scaffoldBackgroundColor: kPrimaryColor,
-        textTheme: GoogleFonts.interTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            // The HomeRepoImplement is the repeated code that can be replaced with a service locator
+            HomeRepoImplement(
+              ApiService(
+                Dio(),
+              ),
+            ),
+          ),
         ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            HomeRepoImplement(
+              ApiService(
+                Dio(),
+              ),
+            ),
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        theme: ThemeData(
+          scaffoldBackgroundColor: kPrimaryColor,
+          textTheme: GoogleFonts.interTextTheme(
+            ThemeData.dark().textTheme,
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        routerConfig: AppRouter.router,
       ),
-      debugShowCheckedModeBanner: false,
-      routerConfig: AppRouter.router,
     );
   }
 }
