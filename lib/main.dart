bbd25c871e09fdcc433c4aff0fe2b07b2b864126
Mohamed Ali <a74498722@gmail.com>
2025-03-08 16:52:11 -0800

    refactor: replace direct instantiation of HomeRepoImplement with service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:book_store_app/constants.dart';
-import 'package:book_store_app/core/utils/api_service.dart';
 import 'package:book_store_app/core/utils/app_router.dart';
+import 'package:book_store_app/core/utils/service_locator.dart';
 import 'package:book_store_app/features/home/data/repos/home_repo_implement.dart';
 import 'package:book_store_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:book_store_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -19,20 +18,12 @@ class BookStore extends StatelessWidget {
         BlocProvider(
           create: (context) => FeaturedBooksCubit(
             // The HomeRepoImplement is the repeated code that can be replaced with a service locator
-            HomeRepoImplement(
-              ApiService(
-                Dio(),
-              ),
-            ),
+            getIt.get<HomeRepoImplement>(),
           ),
         ),
         BlocProvider(
           create: (context) => NewestBooksCubit(
-            HomeRepoImplement(
-              ApiService(
-                Dio(),
-              ),
-            ),
+            getIt.get<HomeRepoImplement>(),
           ),
         ),
       ],
