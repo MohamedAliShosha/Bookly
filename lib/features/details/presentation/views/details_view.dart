33e714a40866948ca0930d0f89cd7a28c9a486e0
Mohamed Ali <a74498722@gmail.com>
2025-03-12 14:35:21 -0700

    refactor: move BookDetailViewBody to a separate widget for improved code organization

--- lib/features/details/presentation/views/details_view.dart
+++ lib/features/details/presentation/views/details_view.dart
@@ -1,8 +1,5 @@
-import 'package:book_store_app/core/utils/styles.dart';
+import 'package:book_store_app/features/details/presentation/widgets/book_details_view_body.dart';
 import 'package:book_store_app/features/home/data/models/book_model/book_model.dart';
-import 'package:book_store_app/features/home/widgets/books_action.dart';
-import 'package:book_store_app/features/home/widgets/custom_book_details_app_bar.dart';
-import 'package:book_store_app/features/home/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
 
 class DetailsView extends StatelessWidget {
@@ -14,82 +11,9 @@ class DetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BookDetailViewBody(bookModel: bookModel),
-    );
-  }
-}
-
-class BookDetailViewBody extends StatelessWidget {
-  const BookDetailViewBody({
-    super.key,
-    required this.bookModel,
-  });
-
-  final BookModel bookModel;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListView(
-      physics: const BouncingScrollPhysics(),
-      children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: CustomBookDetailsAppBar(),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 50),
-              child: SizedBox(
-                height: MediaQuery.of(context).size.height * .4,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: CustomBookImage(
-                    imageUrl: bookModel.volumeInfo.imageLinks?.thumbnail ?? '',
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  maxLines: 3,
-                  overflow: TextOverflow.ellipsis,
-                  bookModel.volumeInfo.title!,
-                  style: Styles.textStyle20,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(
-                  bookModel.volumeInfo.authors?[0] ?? '',
-                  style: Styles.textStyle14,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
-              child: Text(
-                textAlign: TextAlign.center,
-                maxLines: 10,
-                overflow: TextOverflow.ellipsis,
-                bookModel.volumeInfo.description ??
-                    'There is no description available for this book',
-                style: Styles.textStyle14,
-              ),
-            ),
-            BooksAction(
-              bookModel: bookModel,
-            )
-          ],
-        ),
-      ],
+      body: BookDetailViewBody(
+        bookModel: bookModel,
+      ),
     );
   }
 }
---
+++ lib/features/details/presentation/widgets/book_details_view_body.dart
@@ -0,0 +1,81 @@
+import 'package:book_store_app/core/utils/styles.dart';
+import 'package:book_store_app/features/home/data/models/book_model/book_model.dart';
+import 'package:book_store_app/features/home/widgets/books_action.dart';
+import 'package:book_store_app/features/home/widgets/custom_book_details_app_bar.dart';
+import 'package:book_store_app/features/home/widgets/custom_book_image.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailViewBody extends StatelessWidget {
+  const BookDetailViewBody({
+    super.key,
+    required this.bookModel,
+  });
+
+  final BookModel bookModel;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      physics: const BouncingScrollPhysics(),
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: CustomBookDetailsAppBar(),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 50),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * .4,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: CustomBookImage(
+                    imageUrl: bookModel.volumeInfo.imageLinks?.thumbnail ?? '',
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  maxLines: 3,
+                  overflow: TextOverflow.ellipsis,
+                  bookModel.volumeInfo.title!,
+                  style: Styles.textStyle20,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30),
+              child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text(
+                  bookModel.volumeInfo.authors?[0] ?? '',
+                  style: Styles.textStyle14,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+              child: Text(
+                textAlign: TextAlign.center,
+                maxLines: 10,
+                overflow: TextOverflow.ellipsis,
+                bookModel.volumeInfo.description ??
+                    'There is no description available for this book',
+                style: Styles.textStyle14,
+              ),
+            ),
+            BooksAction(
+              bookModel: bookModel,
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
