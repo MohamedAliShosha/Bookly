5c5cdd3f534e35a7ff0bdf35dbc7a562569f38ca
Mohamed Ali <a74498722@gmail.com>
2025-05-30 16:25:49 -0700

    refactor: replace hardcoded color values with ColorsManager.kYellowColor in text styles

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,3 +1,4 @@
+import 'package:book_store_app/core/utils/colors_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -5,32 +6,32 @@ abstract class Styles {
   static const textStyle18 = TextStyle(
     fontSize: 18,
     fontWeight: FontWeight.w600, // SemiBold
-    color: Color(0xffFFEEC1),
+    color: ColorsManager.kYellowColor,
   );
 
   static final textStyle20 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal, // Regular
-    color: const Color(0xffFFEEC1),
+    color: ColorsManager.kYellowColor,
     fontFamily: GoogleFonts.ruslanDisplay().fontFamily,
   );
 
   static final textStyle14 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.normal, // Regular
-    color: const Color(0xffFFEEC1),
+    color: ColorsManager.kYellowColor,
     fontFamily: GoogleFonts.ruslanDisplay().fontFamily,
   );
 
   static const textStyle16 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w500, // Medium
-    color: Color(0xffFFEEC1),
+    color: ColorsManager.kYellowColor,
   );
 
   static const textStyle30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal, // Regular
-    color: Color(0xffFFEEC1),
+    color: ColorsManager.kYellowColor,
   );
 }
