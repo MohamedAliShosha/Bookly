dfd0ad2f999c8c5106539187b532a1913cb571d4
Mohamed Ali <a74498722@gmail.com>
2025-05-29 19:48:00 -0700

    feat: make search icon to be able to navigate to SearchView

--- lib/features/home/presentation/widgets/custom_app_bar.dart
+++ lib/features/home/presentation/widgets/custom_app_bar.dart
@@ -1,5 +1,7 @@
+import 'package:book_store_app/core/utils/app_router.dart';
 import 'package:book_store_app/core/utils/assets_data.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppBar extends StatelessWidget {
   const CustomAppBar({super.key});
@@ -15,10 +17,15 @@ class CustomAppBar extends StatelessWidget {
             height: 30,
           ),
           const Spacer(),
-          const Icon(
-            Icons.search,
-            color: Color(0xffFFEEC1),
-            size: 30,
+          GestureDetector(
+            onTap: () {
+              GoRouter.of(context).push(AppRouter.kSearchView);
+            },
+            child: const Icon(
+              Icons.search,
+              color: Color(0xffFFEEC1),
+              size: 30,
+            ),
           ),
         ],
       ),
