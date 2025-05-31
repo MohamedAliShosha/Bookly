5a01a8f475981e33bba0f8da69b3c2a12bea87ee
Mohamed Ali <a74498722@gmail.com>
2025-05-30 18:00:56 -0700

    feat: implement SearchRepoImplement for fetching search books

---
+++ lib/features/search/data/repos/search_repo_implement.dart
@@ -0,0 +1,39 @@
+import 'package:book_store_app/core/errors/failures.dart';
+import 'package:book_store_app/core/utils/api_service.dart';
+import 'package:book_store_app/features/home/data/models/book_model/book_model.dart';
+import 'package:book_store_app/features/search/data/repos/search_repo.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+
+class SearchRepoImplement implements SearchRepo {
+  final ApiService apiService;
+
+  SearchRepoImplement({required this.apiService});
+  @override
+  Future<Either<Failures, List<BookModel>>> fetchSearchBooks(
+      {required String category}) async {
+    try {
+      var data = await apiService.get(
+          endpoint:
+              'volumes?q=$category&sorting=newest&filtering=free&filtering=free-ebooks');
+
+      List<BookModel> books = []; // Empty list
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return Right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      } else {
+        return left(
+          ServerFailure(
+            e.toString(),
+          ),
+        );
+      }
+    }
+  }
+}
