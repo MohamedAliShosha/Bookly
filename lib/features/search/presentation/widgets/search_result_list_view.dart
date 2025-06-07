a8bb6e1711f15bcbcefb31757062ea3d9648422e
Mohamed Ali <a74498722@gmail.com>
2025-06-06 19:23:12 -0700

    feat: refactor SearchResultListView to use SearchResultListViewItem for better code organization

--- lib/features/search/presentation/widgets/search_result_list_view.dart
+++ lib/features/search/presentation/widgets/search_result_list_view.dart
@@ -2,10 +2,9 @@ import 'package:book_store_app/core/utils/styles.dart';
 import 'package:book_store_app/core/widgets/custom_error_widget.dart';
 import 'package:book_store_app/core/widgets/custom_loading_indicator.dart';
 import 'package:book_store_app/features/search/presentation/manager/SearchBookCubit/search_book_cubit.dart';
-import 'package:book_store_app/features/search/presentation/widgets/custom_book_image2.dart';
+import 'package:book_store_app/features/search/presentation/widgets/search_result_list_view_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class SearchResultListView extends StatelessWidget {
   const SearchResultListView({super.key});
@@ -22,63 +21,7 @@ class SearchResultListView extends StatelessWidget {
             physics: const BouncingScrollPhysics(),
             itemCount: 10,
             itemBuilder: (context, index) {
-              return SizedBox(
-                height: 150,
-                child: Padding(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-                  child: Row(
-                    children: [
-                      const CustomBookImage2(),
-                      const SizedBox(
-                        width: 20,
-                      ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'title',
-                              style: Styles.textStyle20.copyWith(
-                                  fontFamily:
-                                      GoogleFonts.ruslanDisplay().fontFamily),
-                              overflow: TextOverflow
-                                  .ellipsis, // means if text is too long, it will be cut off and replaced with '...'
-                              maxLines: 2,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Text(
-                              'Author',
-                              style: Styles.textStyle14.copyWith(
-                                  fontFamily:
-                                      GoogleFonts.ruslanDisplay().fontFamily),
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            Flexible(
-                              child: Row(
-                                children: [
-                                  Text(
-                                    'Free',
-                                    style: Styles.textStyle20.copyWith(
-                                        fontWeight: FontWeight.bold,
-                                        fontFamily: GoogleFonts.ruslanDisplay()
-                                            .fontFamily),
-                                  ),
-                                  const Spacer(), // Thanks to the expanded widget that i wrapped the column with it, Now I can use Spacer because there will be an available space between the text and the icon
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
+              return const SearchResultListViewItem();
             },
           );
         } else if (state is SearchFailure) {
---
+++ lib/features/search/presentation/widgets/search_result_list_view_item.dart
@@ -0,0 +1,68 @@
+import 'package:book_store_app/core/utils/styles.dart';
+import 'package:book_store_app/features/search/presentation/widgets/custom_book_image2.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SearchResultListViewItem extends StatelessWidget {
+  const SearchResultListViewItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 150,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+        child: Row(
+          children: [
+            const CustomBookImage2(),
+            const SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'title',
+                    style: Styles.textStyle20.copyWith(
+                        fontFamily: GoogleFonts.ruslanDisplay().fontFamily),
+                    overflow: TextOverflow
+                        .ellipsis, // means if text is too long, it will be cut off and replaced with '...'
+                    maxLines: 2,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    'Author',
+                    style: Styles.textStyle14.copyWith(
+                        fontFamily: GoogleFonts.ruslanDisplay().fontFamily),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Flexible(
+                    child: Row(
+                      children: [
+                        Text(
+                          'Free',
+                          style: Styles.textStyle20.copyWith(
+                              fontWeight: FontWeight.bold,
+                              fontFamily:
+                                  GoogleFonts.ruslanDisplay().fontFamily),
+                        ),
+                        const Spacer(), // Thanks to the expanded widget that i wrapped the column with it, Now I can use Spacer because there will be an available space between the text and the icon
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
