2b102c8af0e1c54055848ef3480e8cffc3e9faf5
Mohamed Ali <a74498722@gmail.com>
2025-11-26 11:08:46 -0800

    refactor: Update import statement for book_model

--- lib/features/home/data/repos/home_repo_impl.dart
+++ lib/features/home/data/repos/home_repo_impl.dart
@@ -1,6 +1,7 @@
+import 'package:book_store_app/Features/home/data/models/book_model/book_model.dart';
+
 import '../../../../core/services/api_service.dart';
 
-import '../models/book_model/book_model.dart';
 import 'home_repo.dart';
 import '../../../../core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
