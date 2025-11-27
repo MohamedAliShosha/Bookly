8e74aa609662bd02c2420cfba51620f9287fcd81
Mohamed Ali <a74498722@gmail.com>
2025-11-27 12:52:13 -0800

    refactor(home): Update import for renamed list view widget
    The file `featured_list_view.dart` was renamed to `featured_books_list_view.dart` for better clarity and consistency.
    
    This commit updates the import statement in the `FeaturedBooksListViewBlocConsumer` to reflect the new file name.

--- lib/features/home/presentation/views/widgets/featured_books_list_view_bloc_consumer.dart
+++ lib/features/home/presentation/views/widgets/featured_books_list_view_bloc_consumer.dart
@@ -1,6 +1,6 @@
 import 'package:book_store_app/Features/home/data/models/book_model/book_model.dart';
 import 'package:book_store_app/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
-import 'package:book_store_app/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:book_store_app/Features/home/presentation/views/widgets/featured_books_list_view.dart';
 import 'package:book_store_app/core/function/build_error_snack_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
