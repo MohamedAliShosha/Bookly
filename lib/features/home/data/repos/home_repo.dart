a386c342f9a7bdf1b9040f7918766d9f0126acc9
Mohamed Ali <a74498722@gmail.com>
2025-11-27 11:11:33 -0800

    add page number parameter to the feature book method for pagination

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -5,7 +5,8 @@ import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
   Future<Either<Failure, List<BookModel>>> fetchNewestBooks();
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks(
+      {int pageNumber = 0});
   Future<Either<Failure, List<BookModel>>> fetchSimilarBooks(
       {required String category});
 }
