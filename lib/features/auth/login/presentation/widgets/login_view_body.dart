cfd58e6ee71d111fe5bbd798c49898b4b10baa24
Mohamed Ali <a74498722@gmail.com>
2025-12-10 21:55:51 -0800

    create login view body

---
+++ lib/features/auth/login/presentation/widgets/login_view_body.dart
@@ -0,0 +1,136 @@
+import 'package:book_store_app/core/function/build_enabled_border.dart';
+import 'package:book_store_app/core/function/build_focused_border.dart';
+import 'package:flutter/material.dart';
+
+class LoginViewBody extends StatelessWidget {
+  const LoginViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28, vertical: 80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.lock_outline,
+              size: 85,
+              color: Colors.white,
+            ),
+            const SizedBox(height: 25),
+
+            // Welcome Text
+            const Text(
+              "Welcome Back",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 26,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            // Creates a widget that makes its child partially transparent.
+            const Opacity(
+              opacity: .7,
+              child: Text(
+                "Please sign in to continue",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            const SizedBox(height: 40),
+            // Email Field
+            const CustomTextField(
+              label: "Email",
+              icon: Icons.email_outlined,
+            ),
+            const SizedBox(height: 20),
+
+            // Password Field
+            const CustomTextField(
+              label: "Password",
+              icon: Icons.lock_outline,
+              isPassword: true,
+            ),
+            const SizedBox(height: 40),
+            // Login Button
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+
+            const SizedBox(height: 25),
+
+            // Create Account
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Don’t have an account?",
+                  style: TextStyle(color: Colors.white70, fontSize: 14),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Sign up",
+                    style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 15,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final bool isPassword;
+
+  const CustomTextField({
+    super.key,
+    required this.label,
+    required this.icon,
+    this.isPassword = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: isPassword,
+      style: const TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: Colors.white.withOpacity(.1),
+        labelText: label,
+        labelStyle: const TextStyle(color: Colors.white70),
+        prefixIcon: Icon(icon, color: Colors.white70),
+        enabledBorder: buildEnabledBorder(Colors.grey),
+        focusedBorder: buildFocusedBorder(Colors.white),
+      ),
+    );
+  }
+}
