a91d258f57c0523c63239c72be1bdc4afcb16e36
Mohamed Ali <a74498722@gmail.com>
2025-12-10 21:55:22 -0800

    build enabled and focused border

---
+++ lib/core/function/build_focused_border.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+OutlineInputBorder buildFocusedBorder(Color? borderColor) {
+  return OutlineInputBorder(
+    borderRadius: BorderRadius.circular(14),
+    borderSide: BorderSide(color: borderColor ?? Colors.white),
+  );
+}
